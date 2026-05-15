0560378ccc52be1ff1957c831e68c6edfdcab216
Hatanakaiser <78066093+Hatanakaiser@users.noreply.github.com>
2026-05-15 16:59:08 +0900

    vercel.jsonを追加

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // 資産の読み込みパスを相対パスにする（Vercel単体ならなくても動きます）
 })
